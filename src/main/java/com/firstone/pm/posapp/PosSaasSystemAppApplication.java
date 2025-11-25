cd88527531ed344a032863061fa055954ca9c5e7
Spartak <spartaklysman@gmail.com>
2025-11-25 14:15:09 +0100

    Init commit

---
+++ src/main/java/com/firstone/pm/posapp/PosSaasSystemAppApplication.java
@@ -0,0 +1,13 @@
+package com.firstone.pm.posapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosSaasSystemAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PosSaasSystemAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/firstone/pm/posapp/PosSaasSystemAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstone.pm.posapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosSaasSystemAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
