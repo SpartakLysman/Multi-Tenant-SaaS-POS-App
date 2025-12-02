fef3fab61046d25f4b141c08ca97b48e20fd27b4
Spartak <spartaklysman@gmail.com>
2025-12-02 22:31:31 +0100

    frontend

---
+++ frontend/pop-saas-system-frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import {Button} from "@/components/ui/button.jsx";
+
+function App() {
+
+    return (
+        <>
+<Button> Spartak Lysman </Button>
+
+        </>
+    )
+}
+
+export default App
